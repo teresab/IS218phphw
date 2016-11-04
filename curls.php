7998107afa861d9f224fa6888ef9832d484771f1
Teresa Bembury <tb94@njit.edu>
2016-11-03 20:12:18 -0400

    curls

---
+++ curls.php
@@ -0,0 +1,95 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv=" ">
+    <meta name=" ">
+    <title>Class Assignment _curls</title>
+
+</head>
+<body>
+    <h1>curls</h1>
+ 
+<?php
+
+//GET
+
+function httpGet($url)
+{
+    $ch = curl_init();  
+ 
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+//  curl_setopt($ch,CURLOPT_HEADER, false); 
+ 
+    $output=curl_exec($ch);
+ 
+    curl_close($ch);
+    return $output;
+}
+ 
+echo httpGet("http://hayageek.com");
+
+
+//POST
+
+function httpPost($url,$params)
+{
+  $postData = '';
+   //create name value pairs seperated by &
+   foreach($params as $k => $v) 
+   { 
+      $postData .= $k . '='.$v.'&'; 
+   }
+   $postData = rtrim($postData, '&');
+ 
+    $ch = curl_init();  
+ 
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_HEADER, false); 
+    curl_setopt($ch, CURLOPT_POST, count($postData));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
+ 
+    $output=curl_exec($ch);
+ 
+    curl_close($ch);
+    return $output;
+ 
+}
+//function
+$params = array(
+   "name" => "Ravishanker Kusuma",
+   "age" => "32",
+   "location" => "India"
+);
+ 
+echo httpPost("http://hayageek.com/examples/php/curl-examples/post.php",$params);
+
+
+// ERRORS HANDLING
+
+function httpGetWithErros($url)
+{
+    $ch = curl_init();  
+ 
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+ 
+    $output=curl_exec($ch);
+ 
+    if($output === false)
+    {
+        echo "Error Number:".curl_errno($ch)."<br>";
+        echo "Error String:".curl_error($ch);
+    }
+    curl_close($ch);
+    return $output;
+}
+
+
+
+?>
+</body>
+</html>
